478b440546959c36a27470e4967235cc510ed1e5
acarinhoss <ibrahimacar0121@gmail.com>
2025-04-12 19:12:52 +0300

    Update plant_data.py

--- plant_data.py
+++ plant_data.py
@@ -3,28 +3,23 @@
 plant_dict = {
     "temperature": {
         "0-10": {
-            "spring": [("lettuce", "Once a week", "30 days")],
-            "summer": [],
-            "autumn": [],
+            "spring": [("lettuce", "Once a week", "30 days"), ("spinach", "Once a week", "40 days"), 
+                      ("cabbage", "Once a week", "60 days"), ("radish", "Once a week", "25 days")],
+            "summer": [("lettuce", "Once a week", "30 days"), ("arugula", "Once a week", "25 days")],
+            "autumn": [("cabbage", "Once a week", "60 days"), ("spinach", "Once a week", "40 days"), 
+                      ("radish", "Once a week", "25 days")],
             "winter": [("celery", "Once a week", "90 days"), ("garlic", "Once a week", "120 days")]
         },
         "10-20": {
-            "spring": [("potato", "Twice a week", "90 days"), ("cabbage", "Once a week", "60 days"),
-                       ("lettuce", "Once a week", "30 days"), ("carrot", "Once a week", "75 days")],
-            "summer": [("broccoli", "Once a week", "70 days"), ("cauliflower", "Once a week", "70 days"),
-                       ("lettuce", "Once a week", "30 days")],
-            "autumn": [("carrot", "Once a week", "75 days"), ("cabbage", "Once a week", "60 days"),
-                       ("lettuce", "Once a week", "30 days")],
+            "spring": [("potato", "Twice a week", "90 days"), ("cabbage", "Once a week", "60 days"), ("lettuce", "Once a week", "30 days"), ("carrot", "Once a week", "75 days")],
+            "summer": [("broccoli", "Once a week", "70 days"), ("cauliflower", "Once a week", "70 days"), ("lettuce", "Once a week", "30 days")],
+            "autumn": [("carrot", "Once a week", "75 days"), ("cabbage", "Once a week", "60 days"), ("lettuce", "Once a week", "30 days")],
             "winter": [("celery", "Once a week", "90 days"), ("garlic", "Once a week", "120 days")]
         },
         "15-25": {
-            "spring": [("onion", "Once a week", "90 days"), ("cucumber", "Twice a week", "50 days"),
-                       ("pepper", "Twice a week", "70 days")],
+            "spring": [("onion", "Once a week", "90 days"), ("cucumber", "Twice a week", "50 days"), ("pepper", "Twice a week", "70 days")],
             "summer": [("eggplant", "Twice a week", "80 days"), ("cucumber", "Twice a week", "50 days")],
-            "autumn": [("carrot", "Once a week", "75 days"), ("cauliflower", "Once a week", "70 days"),
-                       ("onion", "Once a week", "90 days"), ("grape", "Once a week", "120 days"),
-                       ("apple", "Once a week", "365 days"), ("pear", "Once a week", "365 days"),
-                       ("cherry", "Once a week", "120 days")],
+            "autumn": [("carrot", "Once a week", "75 days"), ("cauliflower", "Once a week", "70 days"), ("onion", "Once a week", "90 days"), ("grape", "Once a week", "120 days"), ("apple", "Once a week", "365 days"), ("pear", "Once a week", "365 days"), ("cherry", "Once a week", "120 days")],
             "winter": [("cabbage", "Once a week", "60 days"), ("leek", "Once a week", "90 days")]
         },
         "15-30": {
@@ -34,18 +29,18 @@ plant_dict = {
             "winter": [("kale", "Once a week", "60 days"), ("spinach", "Once a week", "40 days")]
         },
         "20-30": {
-            "spring": [("eggplant", "Twice a week", "80 days"), ("tomato", "Twice a week", "70 days"),
-                       ("watermelon", "Twice a week", "90 days")],
+            "spring": [("eggplant", "Twice a week", "80 days"), ("tomato", "Twice a week", "70 days"), ("watermelon", "Twice a week", "90 days")],
             "summer": [("pepper", "Twice a week", "70 days"), ("eggplant", "Twice a week", "80 days")],
-            "autumn": [("carrot", "Once a week", "75 days"), ("onion", "Once a week", "90 days"),
-                       ("plum", "Once a week", "120 days")],
+            "autumn": [("carrot", "Once a week", "75 days"), ("onion", "Once a week", "90 days"), ("plum", "Once a week", "120 days")],
             "winter": [("kale", "Once a week", "60 days"), ("spinach", "Once a week", "40 days")]
         },
         "31-40": {
-            "spring": [],
-            "summer": [],
-            "autumn": [],
-            "winter": []
+            "spring": [("watermelon", "Twice a week", "90 days"), ("melon", "Twice a week", "80 days"),
+                      ("pepper", "Twice a week", "70 days"), ("eggplant", "Twice a week", "80 days")],
+            "summer": [("tomato", "Three times a week", "70 days"), ("eggplant", "Three times a week", "80 days"),
+                      ("pepper", "Three times a week", "70 days"), ("watermelon", "Three times a week", "90 days")],
+            "autumn": [("eggplant", "Twice a week", "80 days"), ("pepper", "Twice a week", "70 days")],
+            "winter": [("heat-resistant spinach", "Once a week", "40 days")]
         }
     },
     "humidity": {
